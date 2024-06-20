a51793e662f44845426c1cd356cd85ba57a14c4e
rishabh90038 <133703005+rishabh90038@users.noreply.github.com>
2024-06-20 13:15:21 +0000

    object destructuring

--- 02_basics_javascript.js/04_objects.js
+++ 02_basics_javascript.js/04_objects.js
@@ -8,12 +8,27 @@ const obj2={
 }
 
 const obj3= Object.assign({},obj1,obj2)  // here we are merging the 2 objets in one
-console.log(obj3)
+///console.log(obj3)
 
 const obj4={...obj1,...obj2}
-console.log(obj4)
+//console.log(obj4)
 
-console.log(Object.keys(obj1))// here we can fetch the keys and value of the object as well using this technique
+//console.log(Object.keys(obj1))// here we can fetch the keys and value of the object as well using this technique
 
 // we can take any object and go to the console, suppose we take an object and go to the console and get the prototype there which tells 
-// different functions ome can use for the objects.
+// different functions ome can use for the objects.// different functions ome can use for the objects.
+
+const cource=
+{
+    name : " mathmatics",
+    pages : 141,
+    takenBY: "rishabh"
+
+}
+
+console.log(cource["takenBY"])
+
+const {takenBY: take}=cource  // here we changed the name we are calling the object value 
+
+console.log(take) // previous it was cource["takeBy"] now it is take, known as destructuring
+
