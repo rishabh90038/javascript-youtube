b6267649eb917c50db7e21807243e97fd7eb438c
rishabh90038 <133703005+rishabh90038@users.noreply.github.com>
2024-06-20 08:27:11 +0000

    datatypes

---
+++ .devcontainer/basic javascript/01_variable.js
@@ -0,0 +1,9 @@
+const accountId=144453
+let accountEmail="@googlr.com"
+var accountPassword="12345"
+accountCity='jaipur'
+//noaccountId=2
+accountName="rishabh"
+
+console.table([accountName,accountCity,accountEmail])
+console.log(accountId);
---
+++ .devcontainer/basic javascript/02_datatypes.js
@@ -0,0 +1,7 @@
+"use strict";// treat code as new version of javascript
+
+//alert(3+3) it can only happens in browser and editor,node js cant run this
+
+let name="rishBH"
+let age=19
+
