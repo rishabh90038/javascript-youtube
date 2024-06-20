a898a981bf0e6e024e0efec5cd35a20f4cc3434b
rishabh90038 <133703005+rishabh90038@users.noreply.github.com>
2024-06-20 10:25:43 +0000

    string basics

--- basic javascript/05_string.js
+++ basic javascript/05_string.js
@@ -1,3 +1,11 @@
 let name = "rishabh"
 console.log(name.length)
 console.log(name.toUpperCase())
+
+//slice can use negative value as it take negative value then it will start from the end.
+// trim- removes the spaces
+// replace can be used to replace things and character from new charcter
+let ask="what is your name"
+console.log(`your question is ${ask}`) // this can be used to insert a value in the string and take it from a variable
+
+
