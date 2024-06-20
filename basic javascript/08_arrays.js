3cee09a7b41906a519f2fd137c9550bfb37e934a
rishabh90038 <133703005+rishabh90038@users.noreply.github.com>
2024-06-20 11:20:29 +0000

    araay

---
+++ basic javascript/08_arrays.js
@@ -0,0 +1,5 @@
+let arr=[2,3,4]
+console.log(arr)
+// we can read about the prperties of the arrays from the javascript documentries.
+
+//slice will not change the original array but splice definetely will.
